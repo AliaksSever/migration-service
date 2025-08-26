7c75f05f4ac7e05de701efabfcf34e326b73868c
Alexey <alexey.severyn@gmail.com>
2025-08-26 11:43:52 +0300

    changelogs been added

---
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+
+--changeset severin:1
+INSERT INTO users (username, email, role)
+VALUES ('user1', 'user1@gmail.com', 'ROLE_USER'),
+       ('admin1', 'admin1@gmail.com', 'ROLE_SUPERVISOR'),
+       ('mentor1', 'mentor1gmail.com', 'ROLE_MENTOR');
